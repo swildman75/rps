2205d2e51a2d63b0c0d74a02310553cacc9ede61
Sean Wildman <sean.wildman75@gmail.com>
2022-12-23 19:54:57 -0500

    Add game() function to app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 // compare user and computer choice for a single game
 // use a for loop to play a 5 round match
 
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3);
   if (randomNum === 0) {
@@ -14,22 +17,35 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
-
-const computerSelection = getComputerChoice();
-
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "It's a draw";
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    playerScore++;
     return `You win, ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    playerScore++;
     return `You win, ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    playerScore++;
     return `You win, ${playerSelection} beats ${computerSelection}`;
   } else {
-    return `You lose ${computerSelection} beats your ${computerSelection}`;
+    computerScore++;
+    return `You lose ${computerSelection} beats your ${playerSelection}`;
   }
 }
 
-playRound(playerSelection, computerSelection);
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt(
+      'Do you choose rock, paper or scissors?'
+    ).toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(
+      `Player Score: ${playerScore} Computer Score: ${computerScore}`
+    );
+  }
+}
+
+console.log(game());
